618807951adbafda4a4a21efcbed9115dd58e692
itsanup1 <96967912+itsanup1@users.noreply.github.com>
2025-08-18 14:46:58 +0530

    Updated amazon clone and added checkout page

---
+++ amazon-clone/scripts/cart.js
@@ -0,0 +1,11 @@
+  export let cart = [{
+    id:"shs2j-sudn3-4hxbc",
+    quantity:2
+  },{
+    id:"f9k7p-q2m8w-r3t5a",
+    quantity:1
+  },{
+    id:"c7x2z-v5b8n-m1k4j",
+    quantity:3
+  }];
+  
---
+++ amazon-clone/scripts/product.js
@@ -0,0 +1,201 @@
+  export const products=[
+    {
+      id:"shs2j-sudn3-4hxbc",
+      image:"images/product-images/6-piece-white-dinner-plate-set.jpg",
+      name:"6 Piece White Dinner Plate Set",
+      rating : {
+        stars:"4.0",
+        count:"37"
+      },
+      price:"2056"
+    },{
+      id:"f9k7p-q2m8w-r3t5a",
+      image:"images/product-images/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+      name:"Adults Plain Cotton T-Shirt-2 Pack",
+      rating : {
+        stars:"4.5",
+        count:"33"
+      },
+      price:"799"
+    },{
+      id:"z1b4c-j6h9k-l2o5p",
+      image:"images/product-images/athletic-cotton-socks-6-pairs.jpg",
+      name:"Black and Grey athletic Cotton Socks-6 Pairs",
+      rating : {
+        stars:"4.5",
+        count:"87"
+      },
+      price:"1090"
+    },{
+      id:"m8n3b-v7x2c-r9a4s",
+      image:"images/product-images/bathroom-rug.jpg",
+      name:"Bathroom Bath Rug Mat 20x31 inch - Grey",
+      rating : {
+        stars:"4.5",
+        count:"119"
+      },
+      price:"1250"
+    },{
+      id:"g5d2e-f1h6j-k8l3i",
+      image:"images/product-images/black-2-slot-toaster.jpg",
+      name:"2 Slot Toaster-Black",
+      rating : {
+        stars:"5.0",
+        count:"2197"
+      },
+      price:"1899"
+    },{
+      id:"p9o4i-u7y2t-r3e6w",
+      image:"images/product-images/blackout-curtains-black.jpg",
+      name:"Blackout Curtains Set 42x84-Inch- Black, 2 Panels",
+      rating : {
+        stars:"4.5",
+        count:"363"
+      },
+      price:"3099"
+    },{
+      id:"c1x8z-v3b6n-m5k2j",
+      image:"images/product-images/coffeemaker-with-glass-carafe-black.jpg",
+      name:"Coffee Maker with Glass Carafe and Reusable",
+      rating : {
+        stars:"4.5",
+        count:"1211"
+      },
+      price:"2250"
+    },{
+      id:"l7j2h-k9g4f-d1s8a",
+      image:"images/product-images/countertop-blender-64-oz.jpg",
+      name:"Countertop Blender- 64oz, 1400 Watts",
+      rating : {
+        stars:"4.0",
+        count:"560"
+      },
+      price:"10747"
+    },{
+      id:"q3w4e-r5t6y-u7i8o",
+      image:"images/product-images/duvet-cover-set-blue-twin.jpg",
+      name:"Duvet Cover Set with Zipper Closure",
+      rating : {
+        stars:"4.0",
+        count:"546"
+      },
+      price:"2399"
+    },{
+      id:"a1s2d-f3g4h-j5k6l",
+      image:"images/product-images/facial-tissue-2-ply-18-boxes.jpg",
+      name:"Ultra Soft Tissue 2-Ply- 18 Box",
+      rating : {
+        stars:"4.0",
+        count:"99"
+      },
+      price:"2374"
+    },{
+      id:"z9x8c-v7b6n-m5k4j",
+      image:"images/product-images/floral-mixing-bowl-set.jpg",
+      name:"10-Piece Mixing Bowl Set with Lids - Floral",
+      rating : {
+        stars:"5.0",
+        count:"679"
+      },
+      price:"3899"
+    },{
+      id:"h3j6k-l9o2p-i5u8y",
+      image:"images/product-images/intermediate-composite-basketball.jpg",
+      name:"Intermediate Size Basketball",
+      rating : {
+        stars:"4.0",
+        count:"128"
+      },
+      price:"2095"
+    },{
+      id:"n1m4b-v7c2x-z9s4d",
+      image:"images/product-images/kitchen-paper-towels-30-pack.jpg",
+      name:"2 Ply Kitchen Paper Towels - 30 Pack",
+      rating : {
+        stars:"4.5",
+        count:"1045"
+      },
+      price:"5799"
+    },{
+      id:"e5r8t-y2u4i-o7p1a",
+      image:"images/product-images/knit-athletic-sneakers-gray.jpg",
+      name:"Men's Knit Athletic sneakers",
+      rating : {
+        stars:"4.0",
+        count:"89"
+      },
+      price:"3390"
+    },{
+      id:"w9q2e-r5t8y-u3i6o",
+      image:"images/product-images/liquid-laundry-detergent-plain.jpg",
+      name:"Liquid Laundry Detergent, 110 loads, 82.5 Fl oz",
+      rating : {
+        stars:"4.5",
+        count:"305"
+      },
+      price:"2899"
+    },{
+      id:"d1f4g-h7j2k-l9p3o",
+      image:"images/product-images/luxury-tower-set-6-piece.jpg",
+      name:"Luxury Towel Set- Graphite Grey",
+      rating : {
+        stars:"5.0",
+        count:"569"
+      },
+      price:"3599"
+    },{
+      id:"b5n8m-v2c6x-z9s3d",
+      image:"images/product-images/men-chino-pants-beige.jpg",
+      name:"Men's Classic Pleated Chino Pants",
+      rating : {
+        stars:"4.0",
+        count:"467"
+      },
+      price:"2290"
+    },{
+      id:"t1y4u-i7o2p-a9s3d",
+      image:"images/product-images/men-cozy-fleece-zip-up-hoodie-red.jpg",
+      name:"Men's Full-Zip Hooded Fleece Sweatshirt",
+      rating : {
+        stars:"4.5",
+        count:"269"
+      },
+      price:"2400"
+    },{
+      id:"k8l5j-h2g9f-d4s1a",
+      image:"images/product-images/men-golf-polo-t-shirt-blue.jpg",
+      name:"Men's Regular-Fit Quick-Dry Golf Polo Shirt",
+      rating : {
+        stars:"4.5",
+        count:"378"
+      },
+      price:"1599"
+    },{
+      id:"o2p5i-u8y1t-r4e7w",
+      image:"images/product-images/men-navigator-sunglasses-brown.jpg",
+      name:"Men's Navigator Sunglasses Pilot",
+      rating : {
+        stars:"5.0",
+        count:"547"
+      },
+      price:"1690"
+    },{
+      id:"r3e6w-q9w2e-r5t8y",
+      image:"images/product-images/men-slim-fit-summer-shorts-gray.jpg",
+      name:"Men's Slim-Fit Summer Shorts",
+      rating : {
+        stars:"4.5",
+        count:"549"
+      },
+      price:"1699"
+    },{
+      id:"c7x2z-v5b8n-m1k4j",
+      image:"images/product-images/plain-hooded-fleece-sweatshirt-yellow.jpg",
+      name:"Plain Hooded Fleece Sweatshirt- Yellow",
+      rating : {
+        stars:"4.0",
+        count:"163"
+      },
+      price:"2400"
+    }
+  ];
