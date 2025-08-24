87f3c31c64a0a2f52383b436adde481847edc3ad
itsanup1 <96967912+itsanup1@users.noreply.github.com>
2025-08-24 23:12:09 +0530

    Updated checkout js and other cart js

--- amazon-clone/scripts/cart.js
+++ amazon-clone/scripts/cart.js
@@ -1,3 +1,7 @@
+  import {products} from "./product.js";
+  
+  //Cart
+  
   export let cart = [{
     id:"shs2j-sudn3-4hxbc",
     quantity:2
@@ -7,5 +11,30 @@
   },{
     id:"c7x2z-v5b8n-m1k4j",
     quantity:3
+  },{
+    id:"r3e6w-q9w2e-r5t8y",
+    quantity:1
+  },{
+    id:"o2p5i-u8y1t-r4e7w",
+    quantity:2
+  },{
+    id:"k8l5j-h2g9f-d4s1a",
+    quantity:5
   }];
+  
+  //Updating The CartInfo
+  
+  export let cartInfo= [];
+  function updateCart(){
+    cart.forEach((item)=>{
+      products.forEach((product,index)=>{
+        if (product.id === item.id){
+          cartInfo.push(products[index]);
+        }
+      });
+      
+    });
+  }
+  
+  updateCart();
   
---
+++ amazon-clone/scripts/checkout.js
@@ -0,0 +1,67 @@
+  import {cartInfo} from "./cart.js";
+  
+  console.log(cartInfo);
+  const orderPreview = document.querySelector('.order-preview');
+  let htmlCode = "";
+  cartInfo.forEach((item)=>{
+    const html = `
+      <div class="order-item">
+        <div class="order-image">
+          <img src= ${item.image}>
+        </div>
+        <div class="order-details">
+          <div class="order-name">
+            ${item.name}
+          </div>
+          <div class="order-price">
+            $${item.price}
+          </div>
+          <div class="order-quantity">
+            Quantity: 2
+          </div>
+        </div>
+        <div class="order-delivery">
+          <div class="order-delivery-title">Choose a delivery option :</div>
+          <div class="order-delivery-option">
+            <input type="radio" name="order1">
+            <div>
+              <div class="order-date">
+                Tuesday, August 26
+              </div>
+              <div class="order-shipping-charge">
+                FREE Shipping
+              </div>
+            </div>
+          </div>
+          <div class="order-delivery-option">
+            <input type="radio" name="order1">
+            <div>
+              <div class="order-date">
+                Tuesday, August 20
+              </div>
+              <div class="order-shipping-charge">
+                $4.99 - Shipping
+              </div>
+            </div>
+          </div>
+          <div class="order-delivery-option">
+            <input type="radio" name="order1">
+            <div>
+              <div class="order-date">
+                Tuesday, August 18
+              </div>
+              <div class="order-shipping-charge">
+                $9.99 - Shipping
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    
+    console.log(html);
+    htmlCode += html;
+    
+  });
+  orderPreview.innerHTML += htmlCode;
+  
